fb4a21194a073bad6aebf92a1f8c83ebd0b17d3e
Aaayang <iamajser@163.com>
2018-09-02 23:42:38 +0800

    配置静态目录、配置模板引擎、连接数据库

---
+++ app.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const config = require('./config');
+
+// 以 public 开头的静态目录
+app.use('/public', express.static('public'));
+
+// 模板配置
+app.engine('html', require('ejs').__express);
+app.set('view engine', 'html');
+
+app.get('/', (req, res) => {
+    res.render('hello', {
+        name: 'aaayang'
+    });
+});
+
+mongoose.connect(config.DB_URL, {
+    useNewUrlParser: true
+}, err => {
+    if(err) {
+        console.log('数据库连接失败');
+    } else {
+        console.log('数据库连接成功 ~');
+        app.listen(config.PORT, () => {
+            console.log(`listening on ${config.PORT}`);
+        });
+    }
+});
